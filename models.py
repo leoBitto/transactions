9ed8539b45b0622ace9a92bef814f08abf2cefdb
leoBitto <leonardobitto@gmail.com>
2023-08-01 22:33:21 +0200

    added login

--- models.py
+++ models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Transaction(models.Model):
+    date= models.DateField()
+    amount = models.FloatField()
+    description=models.CharField(max_length=100)
+
+
+class Income(Transaction):
+    Choices =[
+        ('Savings','Savings'),
+        ('Salary','Salary'),
+        ('Bonus','Bonus'),
+        ('Dividends','Dividends'),
+        ('Other','Other'),
+    ]
+    type=models.CharField(choices=Choices, max_length=10)
+
+
+
+class Expenditure(Transaction):
+    Choices =[
+        ('Food','Food'),
+        ('Gifts','Gifts'),
+        ('Health','Health'),
+        ('House','House'),
+        ('Transport','Transport'),
+        ('Personal Expense','Personal Expense'),
+        ('Bills','Bills'),
+        ('Trip','Trip'),
+        ('Debts','Debts'),
+        ('Other','Other'),
+    ]
+    type=models.CharField(choices=Choices, max_length=16)
--- views.py
+++ views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from .models import *
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+def financial_summary(request):
+    if request.method == 'POST':
+        # Se il form è stato inviato via POST, accedi ai dati inviati dal form
+        start_date = request.POST.get('startDate')  # Assumi che l'input del form abbia il nome 'startDate'
+        end_date = request.POST.get('endDate')  # Assumi che l'input del form abbia il nome 'endDate'
+    else:
+        # Se la richiesta non è una POST, usa delle date di default o gestisci il caso come desideri
+        start_date = '2023-01-01'
+        end_date = '2023-12-31'
+
+    # Filtra le entrate e le spese utilizzando le date specificate o quelle di default
+    incomes = Income.objects.filter(date__range=[start_date, end_date])
+    expenditures = Expenditure.objects.filter(date__range=[start_date, end_date])
+
+    # Stampa i dati a fini di debugging
+    print("start_date:", start_date)
+    print("end_date:", end_date)
+    print("incomes:", incomes)
+    print("expenditures:", expenditures)
+
+    context = {
+        'incomes': incomes,
+        'expenditures': expenditures,
+    }
+
+    # Restituisci la risposta rendendo il template 'transactions/Transactions.html'
+    return render(request, 'transactions/Transactions.html', context)
+@login_required
+def base(request):
+    return render(request, 'transactions/form.html', {})
